ee4d9c0404cbdc6d221446fa1db15ddd55a77c34
jyotipravatripathy <106870217+jyotipravatripathy@users.noreply.github.com>
2022-11-08 11:39:51 +0530

    Add files via upload

---
+++ Student-Management-Security/src/main/java/com/greatLearning/studentManagement/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.greatLearning.studentManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.greatLearning.studentManagement.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    public User getUserByUsername(String username);
+}
